9b8d71e39907740a25d5f87c632683c1c9e5810a
Oli <OliverWDGarrett@gmail.com>
2026-03-19 19:57:46 +1300

    feat: define core event and question schemas

---
+++ src/ev_q_defs.lua
@@ -0,0 +1,58 @@
+local g = require("src.g")
+local reducers = require("src.modules.reducers")
+
+-- Battle events
+g.defineEvent("battleWon")
+g.defineEvent("battleLost")
+g.defineEvent("battleStarted")
+
+-- Entity lifecycle
+g.defineEvent("entitySpawned")
+g.defineEvent("entityDeath")
+
+-- Entity combat
+g.defineEvent("entityHurt")
+g.defineEvent("entityHealed")
+g.defineEvent("entityBuffed")
+g.defineEvent("entityKillsEnemy")
+g.defineEvent("entityShootsProjectile")
+
+-- Squad / deployment
+g.defineEvent("squadDeployed")
+
+-- Spells
+g.defineEvent("spellCast")
+
+-- Economy / run
+g.defineEvent("moneyGained")
+g.defineEvent("rewardChosen")
+
+
+
+
+-- Questions: stat modifiers
+local ADD = reducers.ADD
+local MUL = reducers.MULTIPLY
+
+g.defineQuestion("getMoveSpeedModifier", ADD, 0)
+g.defineQuestion("getDamageModifier", ADD, 0)
+g.defineQuestion("getDamageMultiplier", MUL, 1)
+g.defineQuestion("getMaxHealthModifier", ADD, 0)
+g.defineQuestion("getAttackSpeedMultiplier", MUL, 1)
+g.defineQuestion("getMoveSpeedMultiplier", MUL, 1)
+g.defineQuestion("getRangeModifier", ADD, 0)
+g.defineQuestion("getDamageReduction", ADD, 0)
+-- g.defineQuestion("getArmorMultiplier", MUL, 1) -- TODO: do we even want armor?
+
+-- Questions: spells/mana
+g.defineQuestion("getManaCostMultiplier", MUL, 1)
+g.defineQuestion("getCooldownMultiplier", MUL, 1)
+
+-- Questions: economy/rewards
+g.defineQuestion("getMoneyMultiplier", MUL, 1)
+g.defineQuestion("getRewardChoiceCount", ADD, 3)
+
+-- Questions: projectiles
+g.defineQuestion("getProjectileCountModifier", ADD, 0)
+g.defineQuestion("getProjectileSpeedMultiplier", MUL, 1)
+
