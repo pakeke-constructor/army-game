d1c7d1042235285a6ed901780569233db104409c
Oli <OliverWDGarrett@gmail.com>
2026-03-24 13:58:36 +1300

    feat(ecs): add missing fields to entity component definition

--- src/ecs/components.lua
+++ src/ecs/components.lua
@@ -20,6 +20,19 @@ local ai = {
 ---@class ecs.Entity
 ---@field public ai ecs.components.AI?
 ---@field public color objects.Color?
+---@field public x number
+---@field public y number
+---@field public z number?
+---@field public vx number?
+---@field public vy number?
+---@field public vz number?
+---@field public health number?
+---@field public image string?
+---@field public squad g.Squad?
+---@field public onDeath fun(ecs.Entity)?
+---@field public onSpawn fun(ecs.Entity)?
+---@field public onUpdate fun(ecs.Entity, number)?
+---@field public onDraw fun(ecs.Entity)?
 local ecs_Entity = {}
 
 
