50e8ac6181acb0b4f21f8a65e3ac0ccf615f2b3b
Oli <OliverWDGarrett@gmail.com>
2026-04-07 19:47:56 +1200

    style(g): adjust rarity color values

--- src/g.lua
+++ src/g.lua
@@ -1153,8 +1153,8 @@ end
 ---@class _g._rarities
 g.RARITIES = {
     COMMON = newRarity("COMMON", "COMMON (I)", objects.Color.fromByteRGBA(99,99,99)),
-    UNCOMMON = newRarity("UNCOMMON", "UNCOMMON (II)", objects.Color.fromByteRGBA(53,125,210)),
-    RARE = newRarity("RARE", "RARE (III)", objects.Color.fromByteRGBA(200,82,164)),
+    UNCOMMON = newRarity("UNCOMMON", "UNCOMMON (II)", objects.Color.fromByteRGBA(43,105,180)),
+    RARE = newRarity("RARE", "RARE (III)", objects.Color.fromByteRGBA(160,62,144)),
     LEGENDARY = newRarity("LEGENDARY", "LEGENDARY (IV)", objects.Color.fromByteRGBA(241,241,25)),
 
     UNIQUE = newRarity("UNIQUE", "UNIQUE", objects.Color.WHITE),
