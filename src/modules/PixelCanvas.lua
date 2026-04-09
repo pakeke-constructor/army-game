94738d103042748ba7d59a91a8fc7155f6104f98
Oli <OliverWDGarrett@gmail.com>
2026-04-09 12:45:55 +1200

    docs(PixelCanvas): update usage examples and module documentation

--- src/modules/PixelCanvas.lua
+++ src/modules/PixelCanvas.lua
@@ -7,15 +7,38 @@ Essentially is a big canvas that the user can render to to make their stuff pixe
 It achieves this by creating a smaller canvas that is to be rendered to, and then
 upscaling that canvas to fit the entire screen.
 
-usage:
-
-ps = PixelCanvas(screenWidth, screenHeight)
+## USAGE:
 
 
-ps = PixelCanvas()
+function love.draw()
+    pc = PixelCanvas(screenWidth, screenHeight)
+
+    transform = camera:getTransform() or love.math.newTransform()
+
+    pc:start(transform) -- applies transform, sets canvas.
+
+    renderStuff(...)
+    love.graphics.circle("fill",x,y, 5)
+
+    pc:finish() -- renders canvas, pops transform, etc.
+end
+
+
+-- if start is called twice in a row (no finish) raise error.
 
 
 
+## SPECIAL DETAILS:
+PixelCanvas sounds simple, but there's actually a bit more going on.
+Notably, pixelCanvas objects are meant to be the size of the screen.
+they won't neccessarily render to the entire canvas; only renders to a portion.
+
+then, when ps:finish() is called, the canvas portion is scaled such that it FITS the size of the screen.
+
+The PS objects will also scale their pixels to be the size of scale of THE TRANSFORM.
+
+## Agent instructions:
+All of this is a bit complex; so make sure you do planning and thinking before proceeding.
 
 ]]
 
