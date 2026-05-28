d69ac9ed57d0b023df74d986aa41326b0164744c
Oli <OliverWDGarrett@gmail.com>
2026-05-28 19:33:54 +1200

    perf(blood): optimize splotch storage using RingBuffer

--- src/ecs/systems/blood_system.lua
+++ src/ecs/systems/blood_system.lua
@@ -5,23 +5,21 @@ local HIT_CHANCE = 0.15
 
 local function getStore()
     local world = g.getECS()
-    world.data.bloodSystem = world.data.bloodSystem or {
-        splotches = {},
-    }
+    if not world.data.bloodSystem then
+        world.data.bloodSystem = {
+            splotches = objects.RingBuffer(MAX_SPLOTCHES),
+        }
+    end
     return world.data.bloodSystem
 end
 
 local function spawnSplotch(x, y)
     local store = getStore()
-    local list = store.splotches
-    if #list >= MAX_SPLOTCHES then
-        table.remove(list, 1)
-    end
-    list[#list + 1] = {
+    store.splotches:push({
         x = x + love.math.random(-4, 4),
         y = y + love.math.random(-4, 4),
         rad = love.math.random(3, 7),
-    }
+    })
 end
 
 function blood_system.entityDeath(ent, killer)
@@ -42,10 +40,8 @@ end
 
 function blood_system.preDraw()
     local store = getStore()
-    local list = store.splotches
     lg.setColor(0.35, 0.05, 0.05, 0.65)
-    for i = 1, #list do
-        local s = list[i]
+    for _, s in store.splotches:iter() do
         lg.circle("fill", s.x, s.y, s.rad)
     end
     lg.setColor(1, 1, 1, 1)
