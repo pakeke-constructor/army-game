57e1c3fc4bbe915941769bacdbc7e8f0f121630e
Miku AuahDark <lumi.hotaru@gmail.com>
2026-06-04 12:49:27 +0800

    feat(juice_system): initial healing particles.

--- src/ecs/systems/juice_system.lua
+++ src/ecs/systems/juice_system.lua
@@ -3,7 +3,12 @@ local juiceService = require("src.juiceService")
 local juice_system = {}
 
 local MAX_SPARKS = 20
+local MAX_HEALS = 25
 local HIT_DURATION = 0.16
+local HEAL_SPARKLE = {
+    {"heal_sparkle_1", objects.Color("FFAEE94F")},
+    {"heal_sparkle_2", objects.Color("FF39BC34")},
+}
 
 
 
@@ -33,6 +38,7 @@ local function getStore()
         activeSparks = {},
         sparkPool = {},
         activeHits = {},
+        activeHeals = {},
     }
     return world.data.juiceSystem
 end
@@ -65,6 +71,36 @@ local function spawnHit(store, x, y)
         y = y + love.math.random(-1, 1),
         time = 0,
     }
+end
+
+---@param ent ecs.Entity
+local function spawnHeal(store, ent)
+    if ent.___removed then
+        return
+    end
+
+    local heals = store.activeHeals
+    local t = love.timer.getTime()
+    local heal = nil
+
+    for _, h in ipairs(heals) do
+        if t > h.expire or h.target.___removed then
+            heal = h
+            break
+        end
+    end
+
+    if not heal then
+        if #heals >= MAX_HEALS then
+            return
+        end
+        heal = {}
+        heals[#heals+1] = heal
+    end
+
+    heal.expire = t + 1
+    heal.target = ent
+    heal.seed = love.math.random(0, 65535)
 end
 
 function juice_system.onHitDamage(attacker, damage, target, isArmorHit)
@@ -127,6 +163,14 @@ function juice_system.entityDeath(ent, killer)
     if ent.image and not ent.isPest then
         g.spawnEntity("body", ent.x, ent.y, ent)
     end
+end
+
+---@param unitEnt ecs.Entity
+function juice_system.entityHealed(unitEnt)
+    -- Spawn heal particle
+    print("healed", unitEnt:getTypename())
+    local store = getStore()
+    return spawnHeal(store, unitEnt)
 end
 
 function juice_system.explosion(x, y, damage, radius)
@@ -195,6 +239,30 @@ function juice_system.postDraw()
         local _, _, w, hh = frame:getViewport()
         lg.draw(g.getAtlas(), frame, h.x, h.y, 0, 1, 1, w/2, hh/2)
     end
+
+    local t = love.timer.getTime()
+    local c = gsman.setColor(1, 1, 1)
+    for _, h in ipairs(store.activeHeals) do
+        local remaining = h.expire - t
+        if remaining > 0 and not h.target.___removed then
+            local state = helper.hashInteger(h.seed) * 65536
+
+            for i = 1, 8 do
+                local angle = state / 4294967296 * consts.TAU
+                state = helper.hashInteger(state) * 65536
+                local radius = helper.lerp(0, 25, state / 4294967296)
+                state = helper.hashInteger(state) * 65536
+
+                local imgindex = math.floor(remaining * 8 + i) % 2 + 1
+                local imginfo = HEAL_SPARKLE[imgindex]
+                local x = math.cos(angle) * radius + h.target.x
+                local y = math.sin(angle) * radius + h.target.y - remaining * 10
+                lg.setColor(imginfo[2])
+                g.drawImage(imginfo[1], x, y)
+            end
+        end
+    end
+    c:pop()
 end
 
 return juice_system
