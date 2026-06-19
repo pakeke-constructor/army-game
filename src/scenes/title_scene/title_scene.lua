15a920715460aed90218c338a0cc15bf043815d2
skahds <skahd.dev@gmail.com>
2026-06-19 09:56:58 +0700

    start work on the title scene change

--- src/scenes/title_scene/title_scene.lua
+++ src/scenes/title_scene/title_scene.lua
@@ -44,32 +44,37 @@ function title_scene:draw()
     local lg = love.graphics
     local w, h = lg.getDimensions()
 
+    local spawnX = w * 1/3 -- the middle x of most of the icons and such
+
     lg.clear(0.05, 0.05, 0.07, 1)
 
     lg.setColor(1, 1, 1, 1)
 
-    local titleFont = g.getBigFont(48)
-    local smallFont = g.getSmallFont(16)
+    local titleFont = g.getBigFont(48*3)
+    local smallFont = g.getSmallFont(16*3)
 
-    local titleText = "ARMY GAME"
-    lg.setFont(titleFont)
+    -- local titleText = "ARMY GAME"
+    -- lg.setFont(titleFont)
     lg.setColor(1, 1, 1, 1)
-    local SC=3
-    lg.scale(SC)
-    w,h = w/SC,h/SC
-    lg.print(titleText, (w - titleFont:getWidth(titleText)) / 2, h * 0.3)
+    -- local SC=3
+    -- lg.scale(SC)
+    -- w,h = w/SC,h/SC
+    -- lg.print(titleText, (w - titleFont:getWidth(titleText)) / 2, h * 0.3)
+    local capw, caph = g.getImageSize("smallcapsule")
+    g.drawImage("smallcapsule", spawnX, h * 0.2)
+    
 
     lg.setFont(smallFont)
     local t = love.timer.getTime()
     if math.floor(t * 2) % 2 == 0 then
         local msg = "PRESS ANY KEY"
-        lg.print(msg, (w - smallFont:getWidth(msg)) / 2, h * 0.55)
+        lg.print(msg, spawnX - (smallFont:getWidth(msg)) / 2, h * 0.55)
     end
 
     if consts.DEV_MODE then
         local msg = "(DEV_MODE: Press S to enter sandbox)"
         lg.setColor(objects.Color("FFFFEE00"))
-        lg.print(msg, (w - smallFont:getWidth(msg)) / 2, h * 0.7)
+        lg.print(msg, spawnX - (smallFont:getWidth(msg)) / 2, h * 0.7)
     end
 end
 
