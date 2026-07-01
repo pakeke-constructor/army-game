094c490666403dea5910ad13054c6b7d93c92053
Miku AuahDark <lumi.hotaru@gmail.com>
2026-07-01 09:56:23 +0800

    feat(decor_types): bush decor sway.

--- src/scenes/map_scene/decor_types.lua
+++ src/scenes/map_scene/decor_types.lua
@@ -104,9 +104,19 @@ M.define("brownoak_small", { image = "brownoak_small_1", chance = 0.3, nodeRadiu
 M.define("brownpine_large", { image = "brownpine_large_1", chance = 0.2, nodeRadius = 20, decorRadius = 8, transformModifier = treeAnimation })
 M.define("brownpine_small", { image = "brownpine_small_1", chance = 0.3, nodeRadius = 12, decorRadius = 6, transformModifier = treeAnimation })
 
-M.define("bush_medium", { image = "bush_medium_1", chance = 0.2, nodeRadius = 12, decorRadius = 6})
-M.define("bush_small_1", { image = "bush_small_1", chance = 0.3, nodeRadius = 6, decorRadius = 6})
-M.define("bush_small_2", { image = "bush_small_2", chance = 0.3, nodeRadius = 6, decorRadius = 6})
+
+---@param id integer
+local function bushSway(id)
+    local t = love.timer.getTime()
+    local offt = helper.hashInteger(id) / 65536
+    local a = (t + offt) * math.pi / 4
+    local k = math.sin(a) * math.sin(a * 2) * 0.3
+    return 0, 0, 0, 1, 1, k, 0
+end
+
+M.define("bush_medium", { image = "bush_medium_1", chance = 0.2, nodeRadius = 12, decorRadius = 6, transformModifier = bushSway })
+M.define("bush_small_1", { image = "bush_small_1", chance = 0.3, nodeRadius = 6, decorRadius = 6, transformModifier = bushSway })
+M.define("bush_small_2", { image = "bush_small_2", chance = 0.3, nodeRadius = 6, decorRadius = 6, transformModifier = bushSway })
 
 
 return M
