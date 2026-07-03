a1bf89da606c2741afce962a5cd4ce5002b87988
Miku AuahDark <lumi.hotaru@gmail.com>
2026-07-03 09:34:09 +0800

    update(decor_types): hell dedcoration.

--- src/scenes/map_scene/decor_types.lua
+++ src/scenes/map_scene/decor_types.lua
@@ -66,16 +66,14 @@ local function grassSway(id)
     return 0, 0, 0, 1, 1, k, 0
 end
 
-local GRASS_OPACITY = 0.65
-M.define("grass_1", { image = "grass_1", chance = 0.15, nodeRadius = 4, decorRadius = 8, opacity = GRASS_OPACITY, transformModifier = grassSway })
-M.define("grass_2", { image = "grass_2", chance = 0.3, nodeRadius = 4, decorRadius = 8, opacity = GRASS_OPACITY, transformModifier = grassSway })
-M.define("grass_3", { image = "grass_3", chance = 0.2, nodeRadius = 4, decorRadius = 8, opacity = GRASS_OPACITY, transformModifier = grassSway })
-
-
--- Define built-in decor types
-M.define("mountain_large", { image = "mountain_large_1", chance = 0.1, nodeRadius = 35, decorRadius = 20 })
-M.define("mountain_small_1", { image = "mountain_small_1", chance = 0.3, nodeRadius = 15, decorRadius = 10 })
-M.define("mountain_small_2", { image = "mountain_small_2", chance = 0.1, nodeRadius = 15, decorRadius = 6 })
+---@param id integer
+local function bushSway(id)
+    local t = love.timer.getTime()
+    local offt = helper.hashInteger(id) / 65536
+    local a = (t + offt) * math.pi / 4
+    local k = math.sin(a) * math.sin(a * 2) * 0.3
+    return 0, 0, 0, 1, 1, k, 0
+end
 
 
 ---@param id integer
@@ -95,6 +93,32 @@ local function treeAnimation(id)
     return 0, 0, 0, 1, sy, k, 0
 end
 
+-- Define built-in decor types
+
+-- Grasses
+local GRASS_OPACITY = 0.65
+M.define("grass_1", { image = "grass_1", chance = 0.15, nodeRadius = 4, decorRadius = 8, opacity = GRASS_OPACITY, transformModifier = grassSway })
+M.define("grass_2", { image = "grass_2", chance = 0.3, nodeRadius = 4, decorRadius = 8, opacity = GRASS_OPACITY, transformModifier = grassSway })
+M.define("grass_3", { image = "grass_3", chance = 0.2, nodeRadius = 4, decorRadius = 8, opacity = GRASS_OPACITY, transformModifier = grassSway })
+
+
+-- Mountains
+M.define("mountain_large", { image = "mountain_large_1", chance = 0.1, nodeRadius = 35, decorRadius = 20 })
+M.define("mountain_small_1", { image = "mountain_small_1", chance = 0.3, nodeRadius = 15, decorRadius = 10 })
+M.define("mountain_small_2", { image = "mountain_small_2", chance = 0.1, nodeRadius = 15, decorRadius = 6 })
+
+M.define("hell_mountain_large", { image = "hell_mountain_large_1", chance = 0.1, nodeRadius = 35, decorRadius = 20 })
+M.define("hell_mountain_small_1", { image = "hell_mountain_small_1", chance = 0.3, nodeRadius = 15, decorRadius = 10 })
+M.define("hell_mountain_small_2", { image = "hell_mountain_small_2", chance = 0.1, nodeRadius = 15, decorRadius = 6 })
+M.define("volcano", { image = "volcano_1", chance = 0.1, nodeRadius = 25, decorRadius = 15 })
+M.define("redcrystal_large_1", { image = "redcrystal_large_1", chance = 0.2, nodeRadius = 25, decorRadius = 15 })
+M.define("redcrystal_large_2", { image = "redcrystal_large_2", chance = 0.2, nodeRadius = 20, decorRadius = 15 })
+M.define("redcrystal_medium", { image = "redcrystal_medium_1", chance = 0.2, nodeRadius = 12, decorRadius = 9 })
+M.define("redcrystal_small_1", { image = "redcrystal_small_1", chance = 0.25, nodeRadius = 3, decorRadius = 3 })
+M.define("redcrystal_small_2", { image = "redcrystal_small_1", chance = 0.25, nodeRadius = 3, decorRadius = 3 })
+
+
+-- Trees
 M.define("tree_large_1", { image = "tree_large_1", chance = 0.2, nodeRadius = 20, decorRadius = 8, transformModifier = treeAnimation })
 M.define("tree_small_1", { image = "tree_small_1", chance = 0.3, nodeRadius = 12, decorRadius = 6, transformModifier = treeAnimation })
 -- M.define("tree_small_1_b", { image = "tree_small_1", chance = 0.05, nodeRadius = 8, decorRadius = 4 })
@@ -104,19 +128,17 @@ M.define("brownoak_small", { image = "brownoak_small_1", chance = 0.06, nodeRadi
 M.define("brownpine_large", { image = "brownpine_large_1", chance = 0.25, nodeRadius = 20, decorRadius = 8, transformModifier = treeAnimation })
 M.define("brownpine_small", { image = "brownpine_small_1", chance = 0.35, nodeRadius = 12, decorRadius = 6, transformModifier = treeAnimation })
 
+M.define("demontree", { image = "demontree_1", chance = 0.3, nodeRadius = 20, decorRadius = 8, transformModifier = treeAnimation })
+M.define("burnedtree_1", { image = "burnedtree_1", chance = 0.2, nodeRadius = 10, decorRadius = 8, transformModifier = treeAnimation })
+M.define("burnedtree_2", { image = "burnedtree_2", chance = 0.2, nodeRadius = 10, decorRadius = 8, transformModifier = treeAnimation })
 
----@param id integer
-local function bushSway(id)
-    local t = love.timer.getTime()
-    local offt = helper.hashInteger(id) / 65536
-    local a = (t + offt) * math.pi / 4
-    local k = math.sin(a) * math.sin(a * 2) * 0.3
-    return 0, 0, 0, 1, 1, k, 0
-end
 
+-- Small Trees/Bushes
 M.define("bush_medium", { image = "bush_medium_1", chance = 0.2, nodeRadius = 12, decorRadius = 6, transformModifier = bushSway })
 M.define("bush_small_1", { image = "bush_small_1", chance = 0.3, nodeRadius = 6, decorRadius = 6, transformModifier = bushSway })
 M.define("bush_small_2", { image = "bush_small_2", chance = 0.3, nodeRadius = 6, decorRadius = 6, transformModifier = bushSway })
+M.define("burnedtree_3", { image = "burnedtree_3", chance = 0.2, nodeRadius = 10, decorRadius = 8, transformModifier = bushSway })
+M.define("burnedtree_4", { image = "burnedtree_4", chance = 0.2, nodeRadius = 10, decorRadius = 8, transformModifier = bushSway })
 
 
 return M
