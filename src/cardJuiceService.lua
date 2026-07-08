54a72d74a7fc0874266867d7178bba53823a5378
Miku AuahDark <lumi.hotaru@gmail.com>
2026-07-08 14:38:20 +0800

    update(cardJuiceService): adjust timing.

--- src/cardJuiceService.lua
+++ src/cardJuiceService.lua
@@ -9,6 +9,7 @@ local FALL_UP_VEL = -120
 ---@param i integer Persistent number for "randomness"
 ---@param draw fun(r:kirigami.Region):... draw function with modified region
 function cardJuiceService.drawUnselected(t, r, i, draw)
+    t = helper.clamp(t, 0, 1)
     local cx, cy = r:getCenter()
     local _, screenH = ui.getScaledUIDimensions()
     local dir = (i % 2 == 0) and 1 or -1
@@ -31,7 +32,7 @@ end
 ---@param draw fun(r:kirigami.Region):... draw function with modified region
 ---@param targetR kirigami.Region? Target region for transition?
 function cardJuiceService.drawSelected(t, r, i, draw, targetR)
-    local t1 = helper.EASINGS.sineInOut(t)
+    local t1 = helper.EASINGS.easeOutCubic(helper.clamp(t * 1.5, 0, 1))
     local newR
 
     if targetR then
--- src/ui/choicepanels/common.lua
+++ src/ui/choicepanels/common.lua
@@ -4,7 +4,7 @@ local ChoicePanelCommon = objects.Class("g:ChoicePanelCommon")
 
 ChoicePanelCommon.NUM_CHOICES = 3
 ChoicePanelCommon.FAN_OUT_DURATION = 0.11
-ChoicePanelCommon.SELECT_ANIM_DURATION = 0.5
+ChoicePanelCommon.SELECT_ANIM_DURATION = 0.4
 
 ---@return boolean
 function ChoicePanelCommon:draw()
