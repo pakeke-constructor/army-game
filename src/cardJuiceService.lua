3cbc4aa079bc5e7e3452f546653ea1873041a9cd
Miku AuahDark <lumi.hotaru@gmail.com>
2026-07-08 20:43:40 +0800

    refactor(cardJuiceService): remove stateless rendering function.

--- src/cardJuiceService.lua
+++ src/cardJuiceService.lua
@@ -118,60 +118,6 @@ function CardJuiceInstance:draw()
     return t >= 1
 end
 
----@param t number interpolation time in range [0, 1]
----@param r kirigami.Region original region
----@param i integer Persistent number for "randomness"
----@param draw fun(r:kirigami.Region):... draw function with modified region
-function cardJuiceService.drawUnselected(t, r, i, draw)
-    t = helper.clamp(t, 0, 1)
-    local cx, cy = r:getCenter()
-    local _, screenH = ui.getScaledUIDimensions()
-    local dir = (i % 2 == 0) and 1 or -1
-    local x = dir * (80 + (i % 3) * 24) * t
-    local y = FALL_UP_VEL * t + (screenH + 0.5 * FALL_GRAVITY) * t * t
-    local rot = dir * (2 + (i % 4)) * consts.TAU * 0.25 * t
-    local centeredR = r:moveUnit(-cx, -cy)
-
-    lg.push()
-    lg.translate(cx + x, cy + y)
-    lg.rotate(rot)
-    lg.scale(1 - t)
-    local result = draw(centeredR)
-    lg.pop()
-    return result
-end
-
----@param t number interpolation time in range [0, 1]
----@param r kirigami.Region original region
----@param i integer Persistent number for "randomness"
----@param draw fun(r:kirigami.Region):... draw function with modified region
----@param targetR kirigami.Region? Target region for transition?
-function cardJuiceService.drawSelected(t, r, i, draw, targetR)
-    t = helper.clamp(t * 1.5, 0, 1)
-    local t1 = helper.EASINGS.easeOutCubic(helper.clamp(t, 0, 1))
-    local newR
-
-    if targetR then
-        newR = Kirigami(
-            helper.lerp(r.x, targetR.x, t1),
-            helper.lerp(r.y, targetR.y - parabolic(t, targetR.w) * 0.2, t1),
-            helper.lerp(r.w, targetR.w, t1),
-            helper.lerp(r.h, targetR.h, t1)
-        )
-    else
-        -- Selected goes to center for now
-        local sx, sy = ui.getFullScreenRegion():getCenter()
-        local x, y = r:getCenter()
-
-        local dx = sx - x
-        local dy = sy - y
-        newR = r:moveUnit(dx * t1, dy * t1)
-    end
-
-    -- TODO: Probably some kind of glow/godrays here?
-    return draw(newR)
-end
-
 cardJuiceService.CardJuiceInstance = CardJuiceInstance
 
 return cardJuiceService
