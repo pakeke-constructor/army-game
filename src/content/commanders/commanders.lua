1e5f43018883461645656c081dd4d904ee8d497a
Oli <OliverWDGarrett@gmail.com>
2026-07-10 19:09:54 +1200

    refactor(commanders): centralize starter squad logic using basic squad mapping

--- src/content/commanders/commanders.lua
+++ src/content/commanders/commanders.lua
@@ -1,3 +1,39 @@
+local BASIC_SQUADS = {
+    red = {
+        tank = "gremlin_brute_squad",
+        bruiser = "gremlin_berserker_squad",
+        ranged = "gremlin_slinger_squad",
+    },
+    green = {
+        tank = "human_protector_squad",
+        bruiser = "human_lumberjack_squad",
+        ranged = "green_archer_squad",
+    },
+    blue = {
+        tank = "shield_fish_squad",
+        bruiser = "spear_fish_squad",
+        ranged = "arrow_fish_squad",
+    },
+    yellow = {
+        tank = "protect_bot_squad",
+        bruiser = "angry_bot_squad",
+        ranged = "gun_bot_squad",
+    },
+}
+
+---@param colors string[]
+---@param role string
+local function addBasicSquad(colors, role)
+    local color = colors[love.math.random(#colors)]
+    g.addSquadToArmy(BASIC_SQUADS[color][role])
+end
+
+---@param colors string[]
+local function addBasicStartingSquads(colors)
+    addBasicSquad(colors, "tank")
+    addBasicSquad(colors, "bruiser")
+    addBasicSquad(colors, "ranged")
+end
 
 
 g.defineCommander("sir_horse", "Sir Horse", {
@@ -39,14 +75,7 @@ g.defineCommander("sir_horse", "Sir Horse", {
     },
 
     onStart = function(run)
-        g.addSquadToArmy("red_militia_squad")
-        g.addSquadToArmy("red_archer_squad")
-
-        if consts.DEV_MODE then
-            g.addSpellToArmy("dark_ritual_spell")
-            g.addSpellToArmy("bonereap_spell")
-            g.addSpellToArmy("harrier_spell")
-        end
+        addBasicStartingSquads({"red", "green"})
     end
 })
 
@@ -104,8 +133,7 @@ g.defineCommander("druidcommander", "Druid Lady", {
     },
 
     onStart = function(run)
-        g.addSquadToArmy("green_militia_squad")
-        g.addSquadToArmy("green_archer_squad")
+        addBasicStartingSquads({"red", "green"})
     end
 })
 
@@ -157,8 +185,7 @@ g.defineCommander("mechcommander", "The Mech Goblin", {
     },
 
     onStart = function(run)
-        g.addSquadToArmy("green_militia_squad")
-        g.addSquadToArmy("green_archer_squad")
+        addBasicStartingSquads({"yellow", "green"})
     end
 })
 
@@ -204,8 +231,7 @@ g.defineCommander("lizardcommander", "Lizard Lord", {
     },
 
     onStart = function(run)
-        g.addSquadToArmy("red_militia_squad")
-        g.addSquadToArmy("blue_archer_squad")
+        addBasicStartingSquads({"red", "blue"})
     end
 })
 
@@ -249,7 +275,6 @@ g.defineCommander("octopuscommander", "Octopus Tank", {
     },
 
     onStart = function(run)
-        g.addSquadToArmy("blue_militia_squad")
-        g.addSquadToArmy("blue_archer_squad")
+        addBasicStartingSquads({"blue", "yellow"})
     end
 })
